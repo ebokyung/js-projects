7b1e5a9b8e9ba647de1bf3826ec15f364367d406
ebokyung <90684277+ebokyung@users.noreply.github.com>
2022-01-04 12:51:05 +0900

    update2

--- PWAApp/js/app.js
+++ PWAApp/js/app.js
@@ -4,22 +4,59 @@ console.log( 'testing' );
 $(document).ready( function () 
 {
     var btnLoginTag = $( '.btnLogin' );
+    var loginID = 'tester';
+    var loginPW = '1234';
 
     console.log( 'jQuery document ready.. - ' );
 
     // '$("p")' <-- any p type dom click, hide itself..
+    
+    displayLastLoginTime();
 
-    console.log( 'p how many: ' + $("p").length );
+    testJqueryP();
 
-    $("p").click(function () 
+    btnLoginTag.click( function() 
     {
-        console.log( 'p clicked' );
-        // '$(this)' <-- the p that has been clicked
-        $(this).hide();
+        // Process login here..
+        var input_username_tag = $('.input_username');
+        var input_password_tag = $('.input_password');
+        
+        if(input_username_tag.val() === loginID 
+            && input_password_tag.val() === loginPW)
+        {
+            alert('login success');
+
+            setLastLoginTime( new Date() );
+
+            displayLastLoginTime();
+        }
+        else
+        {
+            alert('login failed');
+        }
+        
     });
 
-    btnLoginTag.click( function() {
-        alert( 'login button clicked' );
-        // Process login here..
-    });
+    // ----------------------------
+    function testJqueryP()
+    {
+        console.log( 'p how many: ' + $("p").length );
+        $("p").click( function() 
+        { 
+            $(this).hide(); 
+        });    
+    };
+
+    // ----------------------------
+
+    function setLastLoginTime( lastLoginTime )
+    {
+        localStorage.setItem( "lastLoginTime", lastLoginTime.toString() );
+    };
+
+    function displayLastLoginTime()
+    {
+        $('.spanLastLoginTime').text( localStorage.getItem('lastLoginTime') );
+    };
+
 });
