eff6f111adaebeea05a4caf634de963e072242cf
James Chang <jamesbchang@gmail.com>
2022-01-04 12:16:59 +0900

    update 1

--- PWAApp/js/app.js
+++ PWAApp/js/app.js
@@ -3,13 +3,23 @@ console.log( 'testing' );
 // '$()' <-- Jquery statement..
 $(document).ready( function () 
 {
+    var btnLoginTag = $( '.btnLogin' );
+
     console.log( 'jQuery document ready.. - ' );
 
     // '$("p")' <-- any p type dom click, hide itself..
+
+    console.log( 'p how many: ' + $("p").length );
+
     $("p").click(function () 
     {
         console.log( 'p clicked' );
         // '$(this)' <-- the p that has been clicked
         $(this).hide();
+    });
+
+    btnLoginTag.click( function() {
+        alert( 'login button clicked' );
+        // Process login here..
     });
 });
